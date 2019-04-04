a3f5520c7bd2b23c7f12eb22438b28c776716dff
Dike90 <919055994@qq.com>
2019-04-04 11:35:30 +0800

    add market_url

--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
-﻿package config
+package config
 
 // API KEY
 const (
@@ -13,13 +13,12 @@ const (
 	// only required when Private Signature is enabled
 	// todo: replace with your own PrivateKey from privatekey.pem
 	PRIVATE_KEY_PRIME_256 string = `xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx`
-
 )
 
 // API请求地址, 不要带最后的/
 const (
 	//todo: replace with real URLs and HostName
-	MARKET_URL string = ""
+	MARKET_URL string = "https://api.huobi.pro"
 	TRADE_URL  string = ""
 	HOST_NAME  string = ""
 )
